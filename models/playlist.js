e8918713cc3d7bcb3990c6ecb9d9f28e19323e46
princeton evans <CadenceMusik@Princetons-Macintosh.local>
2017-12-06 10:18:21 -0600

    changed table on song index

--- models/playlist.js
+++ models/playlist.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const playlistSchema = mongoose.Schema({
   url: String,
-  submitted_by: String
+  submitted_by: String,
+  playlistName: String
 });
 
 module.exports = mongoose.model('Playlist', playlistSchema);
